932a0f8afdb2c240f9c4b34bb75e136dc606f885
YOMAFADA <cleansoul1960>
2021-05-13 01:57:56 +0100

    gatech

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,28 @@
 <?php
+
 namespace App\Providers;
+
 use Illuminate\Support\ServiceProvider;
-//import this
-use Illuminate\Support\Facades\Schema;
-class AppServiceProvider extends ServiceProvider{
-  /**
-  * Register any application services.
-  *
-  * @return void
-  */
-  public function register(){
-    $this->app->singleton(Connection::class, function ($app) {
-        return new Connection(config('riak'));
-  }
-  /**
-  * Bootstrap any application services.
-  *
-  * @return void
-  */
-   public function boot(){
-      //
-   }
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
 }
